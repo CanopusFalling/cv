7ff69153b608c0fe78b1de8410aea684d4b74076
CanopusFalling <kent_sam@outlook.com>
2024-02-12 19:04:40 +0000

    Added the ability to close the header.

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,15 +1,49 @@
+"use client";
+
+import { useState, useEffect } from "react";
 import Link from "next/link";
 
 export default function Header() {
+  const [isHeaderOpen, setIsHeaderOpen] = useState(true);
+
+  useEffect(() => {
+    const storedPreference = localStorage.getItem("headerOpen");
+    console.log(storedPreference);
+    if (storedPreference === "false") {
+      setIsHeaderOpen(false);
+    }
+  }, []);
+
+  function closeHeader() {
+    setIsHeaderOpen(false);
+    localStorage.setItem("headerOpen", "false");
+  }
+
   return (
-    <header className="print:hidden p-2 bg-primary flex flex-row justify-center gap-4">
-      <Link href={"https://github.com/CanopusFalling/cv"} className="underline">
-        View the open source code for this website
-      </Link>
-      |
-      <Link href={"https://canopusfalling.co.uk/"} className="underline">
-        Check out my other projects
-      </Link>
+    <header
+      className={`print:hidden bg-primary flex-row ${
+        isHeaderOpen ? "flex" : "hidden"
+      }`}
+    >
+      <div className="p-2 flex flex-row justify-center gap-4 m-auto">
+        <Link
+          href={"https://github.com/CanopusFalling/cv"}
+          className="underline"
+        >
+          View the open source code for this website
+        </Link>
+        |
+        <Link href={"https://canopusfalling.co.uk/"} className="underline">
+          Check out my other projects
+        </Link>
+      </div>
+      <button
+        className="font-bold m-2"
+        aria-label="close header"
+        onClick={closeHeader}
+      >
+        X
+      </button>
     </header>
   );
 }
