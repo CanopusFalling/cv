d7ff99d716979b36af2b923b4103637da7d6e8dd
CanopusFalling <kent_sam@outlook.com>
2024-02-12 18:22:10 +0000

    Tweaks to printing.

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,10 +15,10 @@ export default function RootLayout({
     <html
       lang="en"
       dir="ltr"
-      className="bg-neutral-200 dark:bg-neutral-800 h-full text-black dark:text-white"
+      className="bg-neutral-200 dark:bg-neutral-800 h-full text-black dark:text-white print:p-0 print:m-0"
     >
       <body>
-        <main className="max-w-3xl mx-auto m-6 p-4 rounded-lg shadow-2xl dark:shadow-neutral-600 dark:shadow-lg print:bg-none print:m-0 print:p-8 print:shadow-none">
+        <main className="max-w-3xl mx-auto m-6 p-4 rounded-lg shadow-2xl dark:shadow-neutral-600 dark:shadow-lg print:bg-none print:m-0 print:p-0 print:shadow-none">
           {children}
         </main>
       </body>
