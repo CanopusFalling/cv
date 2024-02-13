a295f34217d0abb5d8b384fc6ac8a4b49e52ec6f
CanopusFalling <kent_sam@outlook.com>
2024-02-13 01:30:19 +0000

    Space saving tweaks

--- app/components/chip.tsx
+++ app/components/chip.tsx
@@ -9,12 +9,12 @@ interface ChipContainerProps {
 }
 
 export const ChipContainer: React.FC<ChipContainerProps> = ({ children }) => {
-  return <div className="flex flex-wrap gap-2">{children}</div>;
+  return <div className="flex flex-wrap gap-2 gap-y-1">{children}</div>;
 };
 
 const Chip: React.FC<ChipProps> = ({ children }) => {
   return (
-    <div className="bg-neutral-300 shadow-md dark:bg-neutral-700 dark:shadow-neutral-600 p-1 px-4 rounded-full">
+    <div className="bg-neutral-300 shadow-md dark:shadow-sm dark:bg-neutral-700 dark:shadow-neutral-600 p-1 px-4 rounded-full text-sm">
       {children}
     </div>
   );
