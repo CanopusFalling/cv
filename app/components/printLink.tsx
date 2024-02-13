83328730fba74f46e25cc99ab458c6d6fb2e7103
CanopusFalling <kent_sam@outlook.com>
2024-02-13 15:05:51 +0000

    Fixed using window outside of useEffect.

--- app/components/printLink.tsx
+++ app/components/printLink.tsx
@@ -1,21 +1,27 @@
 "use client";
 
-import React, { ReactNode } from "react";
+import React, { ReactNode, useEffect, useState } from "react";
 
 interface PrintLinkProps {
   children: ReactNode;
 }
 
 const PrintLink: React.FC<PrintLinkProps> = ({ children }) => {
-  const link = window.location.href;
+  const [link, setLink] = useState<string | undefined>(undefined);
+
+  useEffect(() => {
+    setLink(window.location.href);
+  }, []); 
 
   return (
     <div className="hidden print:block">
       You can view a fully accessible version of this CV at
-      <a href={link} className="text-primary">
-        {` `}
-        {link}
-      </a>
+      {link && (
+        <a href={link} className="text-primary">
+          {` `}
+          {link}
+        </a>
+      )}
     </div>
   );
 };
