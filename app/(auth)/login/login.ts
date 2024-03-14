d118f1ace8e36114a6499198df60a765d941bcf9
CanopusFalling <kent_sam@outlook.com>
2024-03-14 15:36:52 +0000

    Started work on validation.

--- app/(auth)/login/login.ts
+++ app/(auth)/login/login.ts
@@ -4,9 +4,34 @@ import User from "app/(auth)/User";
 import { redirect } from "next/navigation";
 import { cookies } from "next/headers";
 
-export async function login(formData: FormData): Promise<void> {
+import { SafeParseError, SafeParseReturnType, z } from 'zod';
+
+const schema = z.object({
+  username: z.string({
+    invalid_type_error: 'Invalid Username',
+  }),
+  password: z.string({
+    invalid_type_error: "Invalid Password",
+  })
+})
+
+export async function login(prevState: any, formData: FormData) {
+  console.log(formData.get("password"))
   const username = formData.get("username") as string;
   const password = formData.get("password") as string;
+
+  const validatedFields = schema.safeParse({
+    username: formData.get('username'),
+    password: formData.get("password")
+  })
+
+  if (validatedFields.success === false) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+    }
+  }
+
+
 
   const sessionToken = await generateSession(username, password);
 }
--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,15 +1,24 @@
+"use client";
+
 import { login } from "./login";
+import { useFormState } from "react-dom";
+
+const initialState = {
+  message: "",
+};
 
 export default function LoginPage() {
   const usernameID = "username";
   const passwordID = "current-password";
+
+  const [state, formAction] = useFormState(login, initialState);
 
   return (
     <>
       <h1 className="p-4 text-center w-full text-2xl">Login</h1>
       <form
         className="border p-2 flex flex-col gap-3 max-w-md mx-auto"
-        action={login}
+        action={formAction}
       >
         <section className="flex flex-row gap-2">
           <label htmlFor={usernameID}>Username</label>
@@ -32,6 +41,14 @@ export default function LoginPage() {
             required
           />
         </section>
+        {state.errors && (
+          <div
+            role="alert"
+            className=" bg-red-700 bg-opacity-30 border-l-4 border-red-700 p-2"
+          >
+            {state.errors.username}
+          </div>
+        )}
         <button type="submit">Login</button>
       </form>
     </>
